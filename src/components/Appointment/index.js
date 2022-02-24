2150072c0b9501963eee7e1a747498cf8594efc2
Donatas Tubutis <33428886+dktrdktr@users.noreply.github.com>
2022-02-24 15:42:57 -0500

    props and state in the form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 export default function Appointment(props) {
   const { time } = props;
--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,10 @@ import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -151,5 +155,35 @@ storiesOf("Appointment", module)
       interviewer={interviewer}
       onEdit={action("onEdit")}
       onDelete={action("onDelete")}
+    />
+  ))
+  .add("Confirm", () => (
+    <Confirm
+      message="Delete the appointment?"
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Status", () => <Status message="Deleting" />)
+  .add("Error", () => (
+    <Error
+      message="Could not delete appointment."
+      onClose={action("onClose")}
+    />
+  ))
+  .add("Form Edit", () => (
+    <Form
+      student="Donatas"
+      interviewer={1}
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Form Create", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
     />
   ));
