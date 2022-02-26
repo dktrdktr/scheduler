8515e90c742dda7ee0e39a4168293b7cdb2b3f5c
Donatas Tubutis <33428886+dktrdktr@users.noreply.github.com>
2022-02-26 14:48:53 -0500

    usevisualmode hook in appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,16 +3,40 @@ import "components/Appointment/styles.scss";
 import Show from "components/Appointment/Show";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Form from "components/Appointment/Form";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
+const interviewers = [];
 
 export default function Appointment(props) {
   const { time, interview } = props;
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={time}></Header>
-      {interview ? (
-        <Show student={interview.student} interviewer={interview.interviewer} />
-      ) : (
-        <Empty />
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer}
+          onEdit={console.log("onEdit")}
+          onDelete={console.log("onDelete")}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={interviewers}
+          onSave={console.log("onSave")}
+          onCancel={() => back()}
+        />
       )}
     </article>
   );
